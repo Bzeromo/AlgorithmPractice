c8794d81ed2763255a109c6e9780025587e6a9d7
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-07-21 16:39:48 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+		Scanner sc = new Scanner(System.in);
+		
+		
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		
+		System.out.println(A/B);
+		sc.close();
+	}
+
+}
