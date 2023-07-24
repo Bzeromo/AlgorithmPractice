a0424fc1baf69e48574e3c313add52d7098b8d6a
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-07-24 14:12:52 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int first = 0;
+		for(int i=1; i<=num; i++)
+		{
+			first = first + i;
+		}
+		System.out.println(first);
+		sc.close();
+	}
+
+}
