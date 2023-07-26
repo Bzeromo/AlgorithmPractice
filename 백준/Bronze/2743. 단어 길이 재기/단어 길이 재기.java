00accbd7f1acf062cae31074023d945d0d98a5aa
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-07-26 16:33:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = bf.readLine();
+		
+		bw.write(String.valueOf(str.length()));
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
