caf17903b19ff578dfb272f41ad0aa84f6a37bbf
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-08-07 17:42:52 +0900

    [Bronze V] Title: 검증수, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			int num = sc.nextInt();
+			sum += (num*num);
+		}
+		System.out.println(sum%10);
+	}
+}
+
