1fcc026d30856b83ae12c3803f4088ea61bd0504
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-08-13 16:08:06 +0900

    [Bronze II] Title: 커트라인, Time: 324 ms, Memory: 21228 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int arr [] = new int[N];
+		
+		for(int i =0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j =0; j < N-1; j++) {
+				if(arr[j] < arr[j+1]) {
+					int idx = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = idx;
+				}
+			}
+		}
+		
+		System.out.println(arr[K-1]);
+	}
+
+}
