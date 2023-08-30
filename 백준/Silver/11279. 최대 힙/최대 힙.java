78980231ed4c950cc1d1ba3faa886ed6efab1d39
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-08-30 22:39:02 +0900

    [Silver II] Title: 최대 힙, Time: 328 ms, Memory: 28100 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+			
+		});
+		int N = Integer.parseInt(br.readLine());
+		
+		while(N-- != 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == 0 && !heap.isEmpty()) 
+				sb.append(heap.poll()).append("\n");
+			else if(n==0 && heap.isEmpty())
+				sb.append(String.valueOf(0)).append("\n");
+			else
+				heap.add(n);
+		}
+		
+		bw.write(sb.delete(sb.length()-1, sb.length()-1).toString());
+		bw.close();
+	}
+}
