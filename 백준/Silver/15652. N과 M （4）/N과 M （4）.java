c0377e96202486ad2f12736de9bd9f6411663dfa
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-09-01 17:01:02 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int res [], num [];
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		res = new int [M];
+		num = new int [N];
+		
+		for(int i = 0; i < N; i++) {
+			num[i] = i+1;
+		}
+		
+		comb(0, 0);
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+	
+	static void comb(int idx, int sidx) {
+		if(sidx == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(res[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = idx; i < N; i++) {
+			res[sidx] = num[i];
+			comb(i, sidx+1);
+		}
+	}
+}
