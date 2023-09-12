b67dd8c5d90c6281d1763cb3cfa69dd480571266
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-09-13 01:16:36 +0900

    [Gold IV] Title: 스도쿠, Time: 512 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int sudoku[][];
+    static StringBuilder sb = new StringBuilder();;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        sudoku = new int [9][9];
+        
+        for(int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 9; j++) {
+                sudoku[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        br.close();
+        game(0,0);
+    }
+    
+    static void game(int x, int y) throws IOException {
+        if(y == 9) {
+            game(x+1, 0);
+            return;
+        }
+        
+        if(x == 9) {
+            for(int i = 0; i < 9; i++) {
+                for(int j = 0; j < 9; j++) {
+                    sb.append(sudoku[i][j]).append(" ");
+                    
+                }
+                sb.append("\n");
+            }
+            bw.write(sb.toString());
+            bw.close();
+            System.exit(0);   
+        }
+        
+        if(sudoku[x][y] == 0) {
+            for(int i = 1; i <= 9; i++) {
+                if(isPossible(x, y, i)) {
+                    sudoku[x][y] = i;
+                    game(x, y+1);
+                }
+            }
+            sudoku[x][y] = 0;
+            return;
+        }
+        
+        game(x, y+1);
+    }
+    
+    static boolean isPossible(int x, int y, int num) {
+        for(int i = 0; i < 9; i++) {
+            if(sudoku[i][y] == num)
+                return false;
+        }
+        
+        for(int i = 0; i < 9; i++) {
+            if(sudoku[x][i] == num)
+                return false;
+        }
+        
+        int naemo_x = (x / 3) * 3;
+        int naemo_y = (y / 3) * 3;
+        
+        for(int i = naemo_x; i < naemo_x+3; i++) {
+            for(int j = naemo_y; j < naemo_y+3; j++) {
+                if(sudoku[i][j] == num)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+
+}
