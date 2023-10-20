b9d9412c9da5a1c7367df910f9db7312837b496c
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-10-20 13:25:28 +0900

    [Bronze IV] Title: 녹색거탑, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		bw.write(String.valueOf((int)Math.pow(2, N)));
+		br.close();
+		bw.close();
+	}
+}
