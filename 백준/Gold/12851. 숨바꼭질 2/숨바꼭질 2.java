503a5936235cf660ca2a8f06019732edc013a491
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-12-12 17:39:17 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 212 ms, Memory: 22044 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N, K;
+    static int[] time = new int[100001];
+    static int minTime = 987654321;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if (N >= K) {
+            bw.write((N-K) + "\n1");
+            br.close();
+            bw.close();
+            return;
+        }
+
+        bfs();
+
+        bw.write(minTime + "\n" + count);
+        br.close();
+        bw.close();
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        q.add(N);
+        time[N] = 1;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            if (minTime < time[now]) return;
+
+            for (int i=0; i<3; i++) {
+                int next;
+
+                if (i == 0)         next = now + 1;
+                else if (i == 1)    next = now - 1;
+                else                next = now * 2;
+
+                if (next < 0 || next > 100000) continue;
+
+                if (next == K) {
+                    minTime = time[now];
+                    count++;
+                }
+
+                if (time[next] == 0 || time[next] == time[now] + 1) {
+                    q.add(next);
+                    time[next] = time[now] + 1;
+                }
+            }
+        }
+    }
+}
