0e89a8c4c6f94ff3c041d454dab9e53f6bc1a952
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-12-19 21:47:10 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
