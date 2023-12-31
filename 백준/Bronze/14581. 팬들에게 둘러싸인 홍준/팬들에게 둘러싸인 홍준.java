6c98b3a444fabb785e74cbb3652a56f4e180cb30
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2023-12-31 22:42:06 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String hongjun = sc.nextLine();
+		sc.close();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + hongjun + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
