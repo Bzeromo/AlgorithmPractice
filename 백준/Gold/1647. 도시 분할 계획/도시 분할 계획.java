8f0dfc57464977f07fec8fcb6cb5aaac99ca4c7c
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-02-02 21:21:55 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1624 ms, Memory: 329164 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+	int start;
+	int end;
+	int weight;
+
+	Edge(int start, int end, int weight) {
+		this.start = start;
+		this.end = end;
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Edge o) {
+		return weight - o.weight;
+	}
+
+}
+
+public class Main {
+	static int[] parent;
+	static ArrayList<Edge> edgeList;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		edgeList = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+
+			edgeList.add(new Edge(start, end, weight));
+		}
+
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+
+		Collections.sort(edgeList);
+		
+		// 모든 집이 N - 1개의 길로 연결되도록 만듦.
+		int ans = 0;
+		int bigCost = 0;
+		for (int i = 0; i < edgeList.size(); i++) {
+			Edge edge = edgeList.get(i);
+
+			// 사이클이 발생하는 간선은 제외.
+			if (find(edge.start) != find(edge.end)) {
+				ans += edge.weight;
+				union(edge.start, edge.end);
+				
+				bigCost = edge.weight;
+			}
+		}
+
+		bw.write((ans - bigCost) + "\n");
+		bw.close();
+		br.close();
+	}
+
+	public static int find(int x) {
+		if (x == parent[x]) {
+			return x;
+		}
+
+		return parent[x] = find(parent[x]);
+	}
+
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if (x != y) {
+			parent[y] = x;
+		}
+	}
+
+}
