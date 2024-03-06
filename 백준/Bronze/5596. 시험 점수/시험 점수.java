2e2333067d9ce0f014c3a81c53a5849aa6b96d13
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-03-06 23:06:46 +0900

    [Bronze IV] Title: 시험 점수, Time: 116 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = 0;
+        int T = 0;
+        while (st.hasMoreTokens()) {
+            S += Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            T += Integer.parseInt(st.nextToken());
+        }
+        System.out.print(Math.max(S,T));
+    }
+}
