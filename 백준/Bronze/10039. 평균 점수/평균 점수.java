1edf21e29d70f398116ce44b15de589c6f6695ea
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-03-11 23:21:26 +0900

    [Bronze IV] Title: 평균 점수, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int sum = 0 ;
+		for(int i = 0; i < 5; i++) {
+        
+			int val = in.nextInt();
+			
+			sum += val > 40 ? val : 40;
+			
+			/*
+			  if(val > 40){
+			 	sum += val;
+			  } else {
+			  	sum += 40;
+			  }
+			  
+			*/
+		}
+		System.out.println(sum/5);
+	}
+ 
+}
