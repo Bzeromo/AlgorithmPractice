986baa9d3630b8ce815560bba5ab5bfb04345942
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-03-30 18:31:43 +0900

    [Bronze III] Title: 집 주소, Time: 116 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/1284. 집 주소/집 주소.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//무한루프
+		while(true) {
+			String N = br.readLine();
+			int length = 1;
+			//N이 0이면 반복분을 끝낸다.
+			if(N.equals("0")) {
+				break;
+			}
+			for(int i = 0; i < N.length(); i++) {
+				if(N.charAt(i) == '1') {
+					length += 3;
+				}else if(N.charAt(i) == '0') {
+					length += 5;
+				}else {
+					length += 4;
+				}
+			}
+			System.out.println(length);
+		}
+	}
+
+}
