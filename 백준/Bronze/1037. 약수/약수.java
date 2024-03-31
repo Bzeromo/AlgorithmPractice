87865e42dd7a5d3e67ec99ed25aeb1cff42a9dc5
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-03-31 23:52:47 +0900

    [Bronze I] Title: 약수, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		while(T-- > 0) {
+			int N = in.nextInt();
+			max = N > max ? N : max;
+			min = N < min ? N : min;
+            
+			/*
+			 same this
+			 if(N > max) max = N;
+			 if(N < min) min = N;
+			*/
+		}
+		System.out.println(max * min);
+	}
+ 
+}
