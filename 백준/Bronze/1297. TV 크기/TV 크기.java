60ecd78e339634a804ff002697869bb1395e1888
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-04-26 21:49:51 +0900

    [Bronze II] Title: TV 크기, Time: 152 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double D = Double.parseDouble(st.nextToken());
+		double H = Double.parseDouble(st.nextToken());
+		double W = Double.parseDouble(st.nextToken());
+		
+		double pow = Math.pow(D, 2) / (Math.pow(H, 2) + Math.pow(W, 2));
+		double sqrt = Math.sqrt(pow);
+		
+		System.out.println((int) (sqrt * H) + " " + (int) (sqrt * W));
+	}
+
+}
