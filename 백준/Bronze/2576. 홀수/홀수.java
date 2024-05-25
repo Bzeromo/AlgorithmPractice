568892d9c13ce14750591c2de74515b97fa51dd4
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-05-25 18:36:01 +0900

    [Bronze III] Title: 홀수, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int min = 100;
+		
+		for(int i = 0; i < 7; i++) {
+			int N = Integer.parseInt(br.readLine());
+	
+			if(N % 2 == 1) {
+				sum += N;
+				min = Math.min(min, N);
+			}
+		}
+
+		if(sum == 0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+	}
+
+}
