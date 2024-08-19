76201c5955a6ee92ca356985a472333313fcc853
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-08-19 21:06:36 +0900

    [Gold II] Title: 박스 채우기, Time: 116 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int L = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+ 
+        int N = Integer.parseInt(br.readLine());
+ 
+        int[] cube = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int f = Integer.parseInt(st.nextToken());
+ 
+            cube[t] = f;
+        }
+ 
+        long before = 0; 
+        long ans = 0;
+        for (int i = N - 1; i >= 0; i--) { 
+            before <<= 3;
+            
+            long possibleCube = (long) (L >> i) * (W >> i) * (H >> i) - before;
+            long newCube = Math.min((long) cube[i], possibleCube); 
+ 
+            before += newCube; 
+            ans += newCube;
+        }
+ 
+        if (before == (long) L * W * H) { 
+            bw.write(ans + "\n");
+        } else { 
+            bw.write("-1\n");
+        }
+        bw.close();
+        br.close();
+    }
+ 
+}
