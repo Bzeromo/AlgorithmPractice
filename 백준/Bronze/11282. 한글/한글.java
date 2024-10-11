b47e4795fdada92bb614f02e968d6c9b311fe442
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-10-11 18:16:36 +0900

    [Bronze IV] Title: 한글, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+        
+		N += 44031;
+		char C = (char) N;
+		
+		System.out.println(C);
+        br.close();
+	}
+
+}
