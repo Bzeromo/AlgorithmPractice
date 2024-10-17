15e9acb02f41b2a97cefd0173890fabed22d006c
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2024-10-17 20:09:43 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 116 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];  
+        String s = br.readLine();  
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 97]++;  
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        br.close();
+    }
+}
