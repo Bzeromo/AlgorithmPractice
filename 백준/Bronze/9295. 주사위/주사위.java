4a370a70b2232afab5153ad14e00cb5593dbcac1
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2025-02-06 20:45:40 +0900

    [Bronze IV] Title: 주사위, Time: 168 ms, Memory: 16588 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= x; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case " + i + ": " + (n + m));
+		}
+        br.close();
+	}
+
+}
