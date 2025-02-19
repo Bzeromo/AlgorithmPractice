d103f4617084aa84009033297718840c6d9f766f
young-gue Park <103935448+Bzeromo@users.noreply.github.com>
2025-02-19 20:15:02 +0900

    [Bronze III] Title: 저작권, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int I = Integer.parseInt(st.nextToken());
+        
+		System.out.println(A*(I-1)+1);
+		br.close();
+	}
+}
