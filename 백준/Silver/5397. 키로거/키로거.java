683e0e69c09ddb9a507f90266901849985c8ffb4
Bzeromo <103935448+Bzeromo@users.noreply.github.com>
2025-11-27 23:52:07 +0900

    [Silver II] Title: 키로거, Time: 748 ms, Memory: 83676 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			String line = br.readLine();
+			Stack<Character> pwdStack = new Stack<>();
+			Stack<Character> deleteStack = new Stack<>();
+            
+			int cursor = 0;
+			for(int j = 0; j < line.length(); j++) {
+				char pos = line.charAt(j);
+				if(pos == '<') {
+					if(!pwdStack.isEmpty()) {
+						deleteStack.push(pwdStack.pop());
+					}
+				}else if(pos == '>') {
+					if(!deleteStack.isEmpty()) {
+						pwdStack.push(deleteStack.pop());
+					}
+				}else if(pos == '-') {
+					if(!pwdStack.isEmpty()) {
+						pwdStack.pop();
+					}
+				}else {
+					pwdStack.push(line.charAt(j));
+				}
+			}
+			
+			while(!deleteStack.isEmpty()) {
+				pwdStack.push(deleteStack.pop());
+			}
+			
+			for(int k = 0; k < pwdStack.size(); k++) {
+				bw.write(pwdStack.get(k));
+			}
+			bw.write("\n");
+		}
+		
+		bw.close();
+		br.close();
+	}
+}
